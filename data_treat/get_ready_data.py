cbeef084ee062d75a2a4e434c49170e2de56bcff
roflinyo <evpaden@gmail.com>
2022-06-01 15:27:53 +0300

    .

--- data_treat/get_ready_data.py
+++ data_treat/get_ready_data.py
@@ -1,14 +1,19 @@
 import pandas as pd
+from sqlalchemy import true
 from data_treat.prepare_data import DataCleaner, DataEncoder, Equlizer
 
-def get_data():
+def get_data(full=True):
     a = DataCleaner("bank-additional")
     b = DataEncoder(a.get_data())
-    c = Equlizer(b.get_data())
+    c = Equlizer(b.get_data(), full)
 
 
 
     return c.get_data()
+
+def get_data_for_analiz():
+    a = DataCleaner("bank-additional")
+    return a.get_data()
 
 
 
