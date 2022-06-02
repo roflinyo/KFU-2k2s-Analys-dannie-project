765938ec751eb1bbfcadb85109645d3b41ed6a86
roflinyo <evpaden@gmail.com>
2022-06-02 22:37:57 +0300

    нормализация данных

---
+++ data_treat/data_normalizer.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+class CustomNormalizer:
+
+    def normalize(data: pd.DataFrame):
+        min_max_scaler = MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform(data.values)
+        return pd.DataFrame(x_scaled)
+
+    def standardize(data: pd.DataFrame):
+        min_max_scaler = StandardScaler()
+        x_scaled = min_max_scaler.fit_transform(data.values)
+        return pd.DataFrame(x_scaled)
+    
