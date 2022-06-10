b8ff6c112f9807f79032db8ec694a21a6040b2f1
roflinyo <evpaden@gmail.com>
2022-06-10 15:17:28 +0300

    доделал правило трёх отклонений

--- data_treat/util_class.py
+++ data_treat/util_class.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+COLUMNS = ["distance_from_home","distance_from_last_transaction","ratio_to_median_purchase_price"]
 
 class ThreeQRule:
 
@@ -11,16 +12,16 @@ class ThreeQRule:
 
     def standard_deviation(self):
         val_dict = {}
-        for column in self.data.columns:
+        for column in COLUMNS:
             std = np.std(self.data[column].values, axis=0)
             mean = np.mean(self.data[column].values, axis=0)
             val_dict[column] = (mean - std * 3, mean + std * 3)
         return val_dict
 
     def q3_clear(self, val_dict):
-        for column in self.data.columns:
-            self.data = self.data.drop([val_dict[column][0] > self.data[column].values and \
-                                         self.data[column].values < val_dict[column][1]])
-
+        for column in COLUMNS:
+            self.data = self.data.drop(self.data[self.data[column] < val_dict[column][0]].index)
+            self.data = self.data.drop(self.data[self.data[column] > val_dict[column][1]].index)
+        
     def get_data(self):
         return self.data
